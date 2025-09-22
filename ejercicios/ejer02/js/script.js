4afaa29879315bb86ba0618651759d3754d31f03
DDDBBBPPP <david.bellon.payer@gmail.com>
2025-09-22 11:53:43 +0200

    En casa seguimos

---
+++ ejercicios/ejer02/js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+// 1. Capturamos los elementos del DOM
+const btnClase = document.getElementById("btnClase");
+const btnEtiqueta = document.getElementById("btnEtiqueta");
+// 2. Asignamos funciones a los botones
+btnClase.onclick = resaltarParrafos;
+btnEtiqueta.onclick = resaltarTitulos;
+// 3. Función para resaltar los párrafos usando
+//getElementsByClassName()
+function resaltarParrafos() {
+const parrafos = document.getElementsByClassName("texto");
+for (let i = 0; i < parrafos.length; i++) {
+parrafos[i].style.backgroundColor = "#ffffcc";
+parrafos[i].style.border = "1px solid #ccc";
+parrafos[i].style.padding = "5px";
+}
+}
+
+// 4. Función para resaltar los títulos usando
+//getElementsByTagName()
+function resaltarTitulos() {
+const titulos = document.getElementsByTagName("h3");
+for (let i = 0; i < titulos.length; i++) {
+titulos[i].style.color = "darkred";
+titulos[i].style.textDecoration = "underline";
+}
+}
---
+++ ejercicios/ejer03/js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const btnSelector = document.getElementById("btnSelector");
+const btnSelectorAll = document.getElementById("btnSelectorAll");
+const resultado = document.getElementById("resultado");
+
+btnSelector.addEventListener("click",() =>{
+    const primerParrafo = document.querySelector(".parrafo");
+    //resultado.innerHTML = "<p>querySelector() encontró <b>"+ primerParrafo.textContent + "</b></p>"
+    // Para concatenar, el de arriba o lo de abajo
+    resultado.innerHTML = `<p>querySelector() encontró: <b>${primerParrafo.textContent}</b></p>`;
+});
+
+btnSelectorAll.addEventListener("click",() =>{
+    resultado.innerHTML = "";
+    const allParrafos = document.querySelectorAll(".parrafo");
+    console.log(allParrafos);
+    let salida = `<p>querySelectorAll() encontró: </p><ul>`;
+    allParrafos.forEach((p) => {
+        salida += `<li><b>${p.textContent}</b></li>`;
+    });
+salida += "</ul>";
+
+    resultado.innerHTML = salida;
+});
